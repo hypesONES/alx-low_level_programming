c4ed44e2474be9bc1c8ae414b20cb77c4a49a081
root <Kingsley Bani>
2022-09-13 21:44:14 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,7 +9,7 @@ void print_alphabet(void)
 	char first_alpha;
 	for ( first_alpha = 'a'; first_alpha <= 'z'; first_alpha++)
 	{
-		_putchar(first_alpha)
+		_putchar(first_alpha);
 	}
 
 	_putchar('\n');
