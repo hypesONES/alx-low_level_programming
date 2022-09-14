4cf0c5c8e411e592a2a029b40d1e77c80390d53f
root <Kingsley Bani>
2022-09-14 08:49:57 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - checks if a char is uppercase
+ * @c - character whose case is to be determined
+ *
+ * Return: Returns 1 if uppercase and zero otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
