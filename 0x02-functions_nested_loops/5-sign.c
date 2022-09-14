a81fc9d2596d5ed4d3c67a78891e01f4779a9c95
root <Kingsley Bani>
2022-09-14 13:57:42 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return ('+1');
+		return (1);
 		_putchar('+');
 	}
 	else if (n == 0)
 	{
-		return ('\00');
+		return ('0');
 		_putchar('\0');
 	}
 	else
 	{
-		return ('-1');
+		return (-1);
 		_putchar('-');
 	}
 }
