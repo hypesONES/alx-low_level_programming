5e0142fe02dff871b1725ae1638b83c612a7208c
root <Kingsley Bani>
2022-09-15 05:23:20 -0700

    main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _putchar(char c);
+void print_numbers(void);
+void print_diagonal(int n);
+int _isupper(int c);
+int _isdigit(int c);
+void print _triangle(int size);
+void print_square(int size);
+void print_most_numbers(void);
+void more_numbers(void);
+int mul(int, int);
+void print_line(int n);
+
+#endif
