24feb8cedda8371427a71a99850b099d69817d51
root <Kingsley Bani>
2022-09-15 09:42:15 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,7 +3,7 @@
  * _isdigit - checks if an input is a digit
  * @c: input value to be checked
  *
- * Return: returns 1 for digit and 0 otherwise
+ * Return: Returns 1 for digit and 0 otherwise
  */
 int _isdigit(int c)
 {
