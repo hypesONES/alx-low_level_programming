f968ab8306d4821875b2ec16e33509ef63ff50cd
root <Kingsley Bani>
2022-09-15 15:28:05 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers 10 times
+ *
+ * Returns : void
+ */
+void more_numbers()
+{
+	int count;
+
+	int n;
+
+	for (count = 0; count <= 9; count++)
+	{
+		for (n = 0; n < 15; n++)
+		{
+			_putchar(n);
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
