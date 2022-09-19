246ed155104aba2c299b6f63b5994959c932439b
root <Kingsley Bani>
2022-09-19 16:28:02 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include <main.h>
+/**
+ * _strlen - returns th length of a string
+ * @s address of sring whose length is to be checked
+ *
+ * Return: returns length od string as an integer
+ */
+int _strlen(char *s)
+{
+	int count  = 0;
+
+	char st = *s;
+
+	while (st[count] != '0')
+	{
+		count++;
+	}
+
+	return (count)
+}
