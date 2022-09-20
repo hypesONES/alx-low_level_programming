d919cee53764f2c96cc33d553f354a0256cd1c35
root <Kingsley Bani>
2022-09-19 19:30:01 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ *
+ * Return: returns void
+ */
+void rev_string(char *s)
+{
+	int count  = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	
+	int a = 0;
+	
+	char rev[count];
+
+	while (count > 0)
+	{
+		count--;
+		rev[a] = s[count];
+		a++;
+	}
+	*s = rev;
+}
