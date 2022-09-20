844feed94b5fd4f7da16c1395627365573235ab1
root <Kingsley Bani>
2022-09-19 18:42:59 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,14 @@ void print_rev(char *s)
 {
 	int count  = 0;
 
-	while (s[count] != '\0')                  
+	while (s[count] != '\0')
 	{
-		count++;                                                                }       
-	
+		count++;
+	}
 	while (count >= 0)
 	{
 		_putchar(s[count]);
 		count--;
 	}
+	_putchar('\n');
 }
