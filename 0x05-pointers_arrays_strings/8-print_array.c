af2b57621b772f4caa4b407743382bf380dc53e8
root <Kingsley Bani>
2022-09-19 20:57:21 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - prints arrays on std output
+ * @a: first parameter
+ * @n: second int param
+ *
+ * Return: returns void
+ */
+void print_array(int *a int n)
+{
+	int c;
+
+	for (c = 0; c < n; c++)
+	{
+		printf("%d", a[c]);
+		if (c != (n-1))
+		       printf(", ");	
+	}
+	printf("\n");
+}
