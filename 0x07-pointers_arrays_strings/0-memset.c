23857a959e885ed7a7a0730ed5915ad1b2da5ed6
root <Kingsley Bani>
2022-09-26 13:21:18 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _memset - fills the first n memory point in a buffer with a string
+ * @s: buffer o be filled with n bytes
+ * @b: constant byte
+ * @n: number of bytes to be filled
+ *
+ * Return: returns the pointer to the buffer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		s = s + i;
+		
+		*s[i] = b;
+	}
+
+	return (s);
+}
