9f6673dd661fa8cb123c70de8255038fa077da5d
root <Kingsley Bani>
2022-09-28 21:25:16 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
 		_putchar(*s);
-		_puts_recursion(s + 1);
+		return;
 	}
-	_putchar('\0');
-	_putchar('\n');
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
