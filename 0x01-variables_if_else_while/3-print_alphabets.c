5fca158b6c4bd56737c86007aa547b28cea2d288
root <Kingsley Bani>
2022-10-24 09:36:19 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - print alphabets
+ *
+ * Return: Returns(0) Success
+ */
+int main(void)
+{
+	int a = 97;
+
+	int b  = 65;
+	
+	while (a <= 122)
+	{
+		putchar(a);
+		a++;
+	}
+	while (b <= 90)
+	{
+		putchar(b);
+		b++;
+	}
+
+	reurn (0);
+}
+
