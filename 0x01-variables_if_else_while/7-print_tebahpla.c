00f28a0e60fca26c3e512bbb218b35727a7e450b
root <Kingsley Bani>
2022-10-27 06:48:00 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.>
+/**
+ * main - prints alphabet in lowercase in reverse
+ *
+ * Return: returns (0) - success
+ */
+int main(void)
+{
+	int reverse = 122;
+
+	while (reverse >= 97)
+	{
+		putchar(reverse);
+		reverse--;
+	}
+	putchar('\n');
+	return (0);
+}
