b7489280dc2d7a3c087be257f54b608a4a7ef86b
root <Kingsley Bani>
2022-10-27 08:08:04 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - prints number combinations
+ * 
+ * Return: returns (0) - success
+ */
+int main(void)
+{
+	int num  = 0;
+	
+	while (num <= 9)
+	{
+		if (num != 9)
+		{
+			putchar(num + '0');
+			putchar(' ');
+			putchar(44);
+		}
+		else
+			putchar(num + '0');
+		num++;
+	}
+}
