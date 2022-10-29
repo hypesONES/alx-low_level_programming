e74919d000a40730600b42bfda2c32a37d4448c6
root <Kingsley Bani>
2022-10-29 04:26:34 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+/**
+ * main - print 4 digit combination
+ *
+ * Return - returns (0) - success.
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = 1; j <= 8; j++)
+		{
+			for (k = 2; k <= 9; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i != 7)
+				{
+					putchar(44);
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
