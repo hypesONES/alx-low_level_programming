721d6b91370bec3ca8d9570da263c296940f6da8
root <Kingsley Bani>
2022-11-02 03:10:47 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+/**
+ * main - prints every minutes within 24hrs
+ *
+ * Return: Always returnd (0)- success
+ */
+int main(void)
+{
+	int a, b;
+	
+	for (a = 0; a < 24; a++)
+	{
+		for (b = 0; b < 60; b++)
+		{
+			if (a < 10)
+			{
+				putchar(0 + '0');
+				putchar(a + '0');
+			}
+			else
+				putchar(a + '0');
+			putchar(58);
+			if (b < 10)
+			{
+				putchar(0 + '0');
+				putchar(b + '0');
+			}
+			else
+				putchar(b + '0');
+		}
+	}	
+}
