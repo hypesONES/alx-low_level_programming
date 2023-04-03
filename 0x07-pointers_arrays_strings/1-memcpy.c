947096abc74c81d63a469b415199d96aedafe65c
root <Kingsley Bani>
2023-04-03 14:16:44 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memecpy - copies  fixed memory 
+ * @dest: mem to be copied to
+ * @src: source char to be copied
+ * @n: amount of meme to be copied
+ *
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(dest++) = *(src++);
+		i++;
+	}
+}
